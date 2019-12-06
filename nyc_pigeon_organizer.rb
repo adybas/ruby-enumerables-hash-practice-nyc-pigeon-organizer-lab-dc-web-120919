d42509863a5cb66d8c034ff1686a7e70f8d5f2ac
Anna Dybas <a.m.dybas@gmail.com>
2019-12-06 01:23:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,9 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   binding.pry
   new_hash = {}
-  data.collect do |attribute, data|
-
+  data.each do |attribute, data|
+    data.eacd do |value, name|
+    end
   end
+  new_hash
 end
