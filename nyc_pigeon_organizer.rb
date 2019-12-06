ec00d8ad2ca8d56f582b7994ed837fbef9f1fdaa
Anna Dybas <a.m.dybas@gmail.com>
2019-12-06 01:28:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,10 +2,12 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   new_hash = {}
-  data.each do |attribute, data|
-    data.each do |value, name|
-      name.each do
-        binding.pry
+  data.each do |attribute, data| #attribute => :color; data => different colors
+    data.each do |value, names| # value => :purple ; names => array of names
+      names.each do |name|# => ["Theo", "Peter Jr.", "Lucky"]
+        if !new_hash[name]
+          new_hash[name]
+        end
       end
     end
   end
